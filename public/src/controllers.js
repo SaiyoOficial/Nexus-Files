aa330ec4a8758504c631e4f578712ec4f8ef5fda
Saiyo <noahcodeoficial@proton.me>
2026-06-06 17:19:51 -0300

    Bugfixing
    * Fixed issue that prevented users from downloading music from a.txt file

--- public/src/controllers.js
+++ public/src/controllers.js
@@ -12,9 +12,9 @@ const CheckURL = async ({ url }) => {
     try {
         const Websites = ['www.youtube.com']
         Data = { Success: false, Type: "", InList: false}
-        Websites.includes(new URL(url).hostname) ? Data.InList = true : Data.InList = false 
         url.includes('.txt') ? Data.Type = "file" : Data.Type = "text"
         if (url.includes('.txt')) { Data.Success = true; return Data }
+        Websites.includes(new URL(url).hostname) ? Data.InList = true : Data.InList = false 
         if (Data.InList) { Data.Success = true; return Data }
         console.clear()
         console.log(`\x1b[37m --==--  \x1b[31m${translation[settings.language].files.noinlist}\x1b[37m --==-- \x1b[0m`)
@@ -32,16 +32,16 @@ const CheckURL = async ({ url }) => {
 
 
 async function Youtube(Response, infos) {
+    const Spinner = ora.default("").start()
     try {
         if (Response.Success) {
-            const Spinner = ora.default("").start()
+            
+
             Spinner.color = "yellow"
             if (infos.type == "Sounds") {
                 if (Response.Type == "text") { Command = `"${utils}\\yt-dlp.exe" --yes-playlist -x --audio-format ${infos.format} --ffmpeg-location "${utils}\\ffmpeg.exe" --audio-quality 0 --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json --convert-thumbnails png --ignore-errors --no-abort-on-error --retries 10 --fragment-retries 10 --download-archive "${Cache}\\Songs.txt" --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 15 -o "Download/Songs/%(uploader)s/%(title)s.%(ext)s" "${infos.value}"` }
-                if (Response.type == "file") { Command = `"${utils}\\yt-dlp.exe" -a ${infos.value} --yes-playlist -x --audio-format ${infos.format} --ffmpeg-location "${utils}\\ffmpeg.exe" --audio-quality 0 --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json --convert-thumbnails png --ignore-errors --no-abort-on-error --retries 10 --fragment-retries 10 --download-archive "${Cache}\\Songs.txt" --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 15 -o "Download/Songs/%(uploader)s/%(title)s.%(ext)s"` }
+                if (Response.Type == "file") { Command = `"${utils}\\yt-dlp.exe" -a ${infos.value} --yes-playlist -x --audio-format ${infos.format} --ffmpeg-location "${utils}\\ffmpeg.exe" --audio-quality 0 --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json --convert-thumbnails png --ignore-errors --no-abort-on-error --retries 10 --fragment-retries 10 --download-archive "${Cache}\\Songs.txt" --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 15 -o "Download/Songs/%(uploader)s/%(title)s.%(ext)s"` }
                 Spinner.text = `--> \x1b[32m${translation[settings.language].Songs.Spinner.DownloadingSounds}\x1b[0m <--`
-                await WaitSeconds(1)
-                console.clear()
                 await execPromise(Command, { maxBuffer: 1024 * 1024 * 50 })
                 Spinner.succeed(`--==-- \x1b[32m${translation[settings.language].Songs.Spinner.FullDownloads}\x1b[0m --==--`)
                 await WaitSeconds(3)
@@ -52,8 +52,6 @@ async function Youtube(Response, infos) {
                 if (Response.Type == "text") { Command = `"${utils}\\yt-dlp.exe" --ffmpeg-location "${utils}" --yes-playlist -f "bv*+ba/b" --merge-output-format ${infos.format} --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json --convert-thumbnails png --ignore-errors --no-abort-on-error --retries 10 --fragment-retries 10 --download-archive "${Cache}\\Videos.txt" --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 15 -o "Download/Videos/%(uploader)s/%(title)s.%(ext)s" "${infos.value}"` }
                 if (Response.Type == "file") { Command = `"${utils}\\yt-dlp.exe" --ffmpeg-location "${utils}" -a ${infos.value} --yes-playlist -f "bv*+ba/b" --merge-output-format ${infos.format} --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json --convert-thumbnails png --ignore-errors --no-abort-on-error --retries 10 --fragment-retries 10 --download-archive "${Cache}\\Videos.txt" --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 15 -o "Download/Videos/%(uploader)s/%(title)s.%(ext)s"` }
                 Spinner.text = `--> \x1b[32m${translation[settings.language].Videos.Spinner.DownloadingVideos}\x1b[0m <--`
-                await WaitSeconds(1)
-                console.clear()
                 await execPromise(Command, { maxBuffer: 1024 * 1024 * 50 })
                 Spinner.succeed(`--==-- \x1b[32m${translation[settings.language].Videos.Spinner.FullDownloads}\x1b[0m --==--`)
                 await WaitSeconds(3)
@@ -61,17 +59,18 @@ async function Youtube(Response, infos) {
 
         }
     } catch (error) {
+        console.log(error)
         await WaitSeconds(3)
         console.clear()
         if (Retry > 0) {
             Retry--
-            console.log(`\x1b[37m --==--  \x1b[31m${translation[settings.language].Error.Retry}\x1b[37m --==-- \x1b[0m`)
+            Spinner.fail(`\x1b[37m --==--  \x1b[31m${translation[settings.language].Error.Retry} ${Retry + 1} / 3 \x1b[37m --==-- \x1b[0m`)
             await WaitSeconds(3)
             console.clear()
             await WaitSeconds(0.5)
-            this.DownloadSounds({ format, url })
+            await Donwload(infos)
         } else {
-            console.log(`\x1b[37m --==--  \x1b[31m${translation[settings.language].Error.MaxRetry}\x1b[37m --==-- \x1b[0m`)
+            Spinner.fail(`\x1b[37m --==--  \x1b[31m${translation[settings.language].Error.MaxRetry}\x1b[37m --==-- \x1b[0m`)
             await WaitSeconds(5)
             Retry = 3
             console.clear()
