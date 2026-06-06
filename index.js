ce1b8d7f3bf14b76081551e0ad047980d82db2c4
Saiyo <noahcodeoficial@proton.me>
2026-06-06 16:33:10 -0300

    Codigo refactoring
    * Improvement in the general code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require("path")
-const language = require("./public/json/language.json")
+const translation = require("./public/json/translation.json")
 const settings = require("./public/json/settings.json")
 const controllers = require("./public/src/controllers")
 const inquirer = require("@inquirer/prompts")
@@ -7,56 +7,57 @@ const fs = require("fs")
 
 async function __init__() {
     let response
-    if (!fs.existsSync("./Cache")) { fs.mkdirSync("./Cache", { recursive: true }) }
-    if (!fs.existsSync("./Cache/Songs.txt")) { fs.writeFileSync("./Cache/Songs.txt", "", "utf-8") }
-    if (!fs.existsSync("./Cache/Videos.txt")) { fs.writeFileSync("./Cache/Videos.txt", "", "utf-8") }
-
     while (true) {
+        new Promise((resolver,reject)=>{setInterval(()=>{
+          if (!fs.existsSync("./Cache")) { fs.mkdirSync("./Cache", { recursive: true }) }
+        if (!fs.existsSync("./Cache/Songs.txt")) { fs.writeFileSync("./Cache/Songs.txt", "", "utf-8") }
+        if (!fs.existsSync("./Cache/Videos.txt")) { fs.writeFileSync("./Cache/Videos.txt", "", "utf-8") }
+        },1000)})
         let init = await inquirer.select({
-            message: language[settings.language].Init.message,
-            choices: language[settings.language].Init.choices.map((name, index) => ({ name: name, value: ["ENTER", "Settings"][index] }))
+            message: translation[settings.language].Init.message,
+            choices: translation[settings.language].Init.choices.map((name, index) => ({ name: name, value: ["ENTER", "Settings"][index] }))
         })
         if (init == "Settings") {
             console.clear()
             let Settings = await inquirer.select({
-                message: language[settings.language].Settings.message,
-                choices: language[settings.language].Settings.choices.map((name, index) => ({ name: name, value: ["Language", "Additional settings"][index] }))
+                message: translation[settings.language].Settings.message,
+                choices: translation[settings.language].Settings.choices.map((name, index) => ({ name: name, value: ["Language", "Additional settings"][index] }))
             })
             if (Settings == "Language") {
                 let Data = []
-                for (l in language) {
+                for (l in translation) {
                     Data.push(l)
                 }
                 let LanguageSelect = await inquirer.select({
-                    message: language[settings.language].Language.message,
+                    message: translation[settings.language].Language.message,
                     choices: Data
                 })
                 settings.language = LanguageSelect
-                fs.writeFileSync(`${path.join(__dirname,"public","json","settings.json")}`, `{"language":"${LanguageSelect}"}`)
+                fs.writeFileSync(`${path.join(__dirname, "public", "json", "settings.json")}`, `{"language":"${LanguageSelect}"}`)
                 console.clear()
                 continue
             }
         }
 
         response = await inquirer.select({
-            message: language[settings.language].Home.message,
-            choices: language[settings.language].Home.choices.map((name, index) => ({ name: name, value: ["Videos", "Songs", "Anime", "Books"][index] })),
+            message: translation[settings.language].Home.message,
+            choices: translation[settings.language].Home.choices.map((name, index) => ({ name: name, value: ["Videos", "Songs", "Anime", "Books"][index] })),
         })
         switch (response) {
             case "Videos":
-                format = await inquirer.select({ message: language[settings.language].Videos.FormatMessage, choices: ["mkv", "mp4"] })
-                url = await inquirer.input({ message: language[settings.language].Videos.message })
-                await controllers.DownloadVideos({ url, format })
+                format = await inquirer.select({ message: translation[settings.language].Videos.FormatMessage, choices: ["mkv", "mp4"] })
+                value = await inquirer.input({ message: translation[settings.language].Videos.message })
+                await controllers.Donwload({ value, format ,type:"Videos"})
                 break
 
             case "Songs":
-                format = await inquirer.select({ message: language[settings.language].Songs.FormatMessage, choices: ["mp3"] })
-                url = await inquirer.input({ message: language[settings.language].Songs.message })
-                await controllers.DownloadSounds({ url, format })
+                format = await inquirer.select({ message: translation[settings.language].Songs.FormatMessage, choices: ["mp3"] })
+                value = await inquirer.input({ message: translation[settings.language].Songs.message })
+                await controllers.Donwload({ value, format, type:"Sounds" })
                 break
 
             default:
-                console.log(language[settings.language].Error.Default)
+                console.log(translation[settings.language].Error.Default)
         }
     }
 
--- public/src/controllers.js
+++ public/src/controllers.js
@@ -1,120 +1,96 @@
-const language = require("../json/language.json")
+const translation = require("../json/translation.json")
 const settings = require("../json/settings.json")
 const ora = require("ora")
 const path = require("path")
 const execPromise = require("util").promisify(require("child_process").exec)
-const WaitSeconds = (ms) => new Promise((r) => { setInterval(r, ms) })
+const WaitSeconds = (ms) => new Promise((r) => { setInterval(r, ms * 1000) })
 const utils = path.join(__dirname, "../utils")
 const Cache = "./Cache"// path.join(__dirname, "../../Cache")
+let Data 
 let Retry = 3
 const CheckURL = async ({ url }) => {
-    const Websites = ['www.youtube.com']
-    const Data = { Type: "", InList: false }
-    url.includes('.txt') ? Data.Type = "file" : Data.Type = "text"
-    if (url.includes('.txt')) { return { Success: true, Type: Data.Type, message: null } }
     try {
-        Websites.includes(new URL(url).hostname) ? Data.InList = true : Data.InList = false
-        if (Data.InList == true) { return { Success: true, Type: Data.Type, message: null } } else {
-            console.clear()
-            console.log(`\x1b[37m --==--  \x1b[31m${language[settings.language].files.noinlist}\x1b[37m --==-- \x1b[0m`)
-            await WaitSeconds(10000)
-            console.clear()
-            return { Success: false, Type: Data.Type }
-        }
-    } catch {
+        const Websites = ['www.youtube.com']
+        Data = { Success: false, Type: "", InList: false}
+        Websites.includes(new URL(url).hostname) ? Data.InList = true : Data.InList = false 
+        url.includes('.txt') ? Data.Type = "file" : Data.Type = "text"
+        if (url.includes('.txt')) { Data.Success = true; return Data }
+        if (Data.InList) { Data.Success = true; return Data }
         console.clear()
-        console.log(`\x1b[37m --==--  \x1b[31m${language[settings.language].files.UrlOnly}\x1b[37m --==--\x1b[0m`)
-        await WaitSeconds(5000)
+        console.log(`\x1b[37m --==--  \x1b[31m${translation[settings.language].files.noinlist}\x1b[37m --==-- \x1b[0m`)
+        await WaitSeconds(10)
         console.clear()
-        return { Success: false, Type: Data.Type }
+        return Data
+    } catch (error) {
+        console.clear()
+        console.log(`\x1b[37m --==--  \x1b[31m${translation[settings.language].Error.Default}\x1b[37m --==--\x1b[0m`)
+        await WaitSeconds(5)
+        console.clear()
+        return Data
     }
 }
 
+
+async function Youtube(Response, infos) {
+    try {
+        if (Response.Success) {
+            const Spinner = ora.default("").start()
+            Spinner.color = "yellow"
+            if (infos.type == "Sounds") {
+                if (Response.Type == "text") { Command = `"${utils}\\yt-dlp.exe" --yes-playlist -x --audio-format ${infos.format} --ffmpeg-location "${utils}\\ffmpeg.exe" --audio-quality 0 --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json --convert-thumbnails png --ignore-errors --no-abort-on-error --retries 10 --fragment-retries 10 --download-archive "${Cache}\\Songs.txt" --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 15 -o "Download/Songs/%(uploader)s/%(title)s.%(ext)s" "${infos.value}"` }
+                if (Response.type == "file") { Command = `"${utils}\\yt-dlp.exe" -a ${infos.value} --yes-playlist -x --audio-format ${infos.format} --ffmpeg-location "${utils}\\ffmpeg.exe" --audio-quality 0 --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json --convert-thumbnails png --ignore-errors --no-abort-on-error --retries 10 --fragment-retries 10 --download-archive "${Cache}\\Songs.txt" --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 15 -o "Download/Songs/%(uploader)s/%(title)s.%(ext)s"` }
+                Spinner.text = `--> \x1b[32m${translation[settings.language].Songs.Spinner.DownloadingSounds}\x1b[0m <--`
+                await WaitSeconds(1)
+                console.clear()
+                await execPromise(Command, { maxBuffer: 1024 * 1024 * 50 })
+                Spinner.succeed(`--==-- \x1b[32m${translation[settings.language].Songs.Spinner.FullDownloads}\x1b[0m --==--`)
+                await WaitSeconds(3)
+                console.clear()
+            }
+
+            if (infos.type == "Videos") {
+                if (Response.Type == "text") { Command = `"${utils}\\yt-dlp.exe" --ffmpeg-location "${utils}" --yes-playlist -f "bv*+ba/b" --merge-output-format ${infos.format} --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json --convert-thumbnails png --ignore-errors --no-abort-on-error --retries 10 --fragment-retries 10 --download-archive "${Cache}\\Videos.txt" --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 15 -o "Download/Videos/%(uploader)s/%(title)s.%(ext)s" "${infos.value}"` }
+                if (Response.Type == "file") { Command = `"${utils}\\yt-dlp.exe" --ffmpeg-location "${utils}" -a ${infos.value} --yes-playlist -f "bv*+ba/b" --merge-output-format ${infos.format} --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json --convert-thumbnails png --ignore-errors --no-abort-on-error --retries 10 --fragment-retries 10 --download-archive "${Cache}\\Videos.txt" --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 15 -o "Download/Videos/%(uploader)s/%(title)s.%(ext)s"` }
+                Spinner.text = `--> \x1b[32m${translation[settings.language].Videos.Spinner.DownloadingVideos}\x1b[0m <--`
+                await WaitSeconds(1)
+                console.clear()
+                await execPromise(Command, { maxBuffer: 1024 * 1024 * 50 })
+                Spinner.succeed(`--==-- \x1b[32m${translation[settings.language].Videos.Spinner.FullDownloads}\x1b[0m --==--`)
+                await WaitSeconds(3)
+            }
+
+        }
+    } catch (error) {
+        await WaitSeconds(3)
+        console.clear()
+        if (Retry > 0) {
+            Retry--
+            console.log(`\x1b[37m --==--  \x1b[31m${translation[settings.language].Error.Retry}\x1b[37m --==-- \x1b[0m`)
+            await WaitSeconds(3)
+            console.clear()
+            await WaitSeconds(0.5)
+            this.DownloadSounds({ format, url })
+        } else {
+            console.log(`\x1b[37m --==--  \x1b[31m${translation[settings.language].Error.MaxRetry}\x1b[37m --==-- \x1b[0m`)
+            await WaitSeconds(5)
+            Retry = 3
+            console.clear()
+        }
+    }
+}
+
+
+async function Donwload(infos) {
+    let Command
+    const Response = await CheckURL({ url: infos.value })
+    await Youtube(Response, infos)
+    console.clear()
+}
+
+
+
+
+
 module.exports = {
-
-    async DownloadSounds({ url, format }) {
-        const Verify = await CheckURL({ url })
-        if (Verify.Success == true) {
-            const Spinner = ora.default("").start()
-            if (Verify.Type == "text") {
-                Spinner.color = "yellow"
-                Spinner.text = `--> \x1b[32m${language[settings.language].Songs.Spinner.DownloadingSounds}\x1b[0m <--`
-                await execPromise(`"${utils}\\yt-dlp.exe" --yes-playlist -x --audio-format ${format} --ffmpeg-location "${utils}\\ffmpeg.exe" --audio-quality 0 --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json --convert-thumbnails png --ignore-errors --no-abort-on-error --retries 10 --fragment-retries 10 --download-archive "${Cache}\\Songs.txt" --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 15 -o "Download/Songs/%(uploader)s/%(title)s.%(ext)s" "${url}"`,{ maxBuffer: 1024 * 1024 * 50 })
-                Spinner.succeed(`--==-- \x1b[32m${language[settings.language].Songs.Spinner.FullDownloads}\x1b[0m --==--`)
-                await WaitSeconds(3000)
-                console.clear()
-            }
-
-            if (Verify.Type == "file") {
-                Spinner.color = "yellow"
-                Spinner.text = `--> \x1b[32m${language[settings.language].Songs.Spinner.DownloadingSounds}\x1b[0m <--`
-                try { await execPromise(`"${utils}\\yt-dlp.exe" -a ${url} --yes-playlist -x --audio-format ${format} --ffmpeg-location "${utils}\\ffmpeg.exe" --audio-quality 0 --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json --convert-thumbnails png --ignore-errors --no-abort-on-error --retries 10 --fragment-retries 10 --download-archive "${Cache}\\Songs.txt" --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 15 -o "Download/Songs/%(uploader)s/%(title)s.%(ext)s"`, { maxBuffer: 1024 * 1024 * 50 }) }
-                catch {
-                    Spinner.fail(language[settings.language].Error.Default)
-                    await WaitSeconds(3000)
-                    console.clear()
-                    if (Retry > 0) {
-                        Retry-- 
-                        console.log(`\x1b[37m --==--  \x1b[31m${language[settings.language].Error.Retry}\x1b[37m --==-- \x1b[0m`)
-                        await WaitSeconds(3000)
-                        console.clear()
-                        await WaitSeconds(500)
-                        this.DownloadSounds({ format, url })
-                    } else {
-                        console.log(`\x1b[37m --==--  \x1b[31m${language[settings.language].Error.MaxRetry}\x1b[37m --==-- \x1b[0m`)
-                        await WaitSeconds(5000)
-                        Retry = 3
-                        console.clear()
-                    }
-                }
-                Spinner.succeed(`--==-- \x1b[32m${language[settings.language].Songs.Spinner.FullDownloads}\x1b[0m --==--`)
-            }
-        }
-    },
-
-    async DownloadVideos({ url, format }) {
-        const Verify = await CheckURL({ url })
-        if (Verify.Success == true) {
-            const Spinner = ora.default("").start()
-            if (Verify.Type == "text") {
-                Spinner.color = "yellow"
-                Spinner.text = `--> \x1b[32m${language[settings.language].Videos.Spinner.DownloadingVideos}\x1b[0m <--`
-                await execPromise(`"${utils}\\yt-dlp.exe" --ffmpeg-location "${utils}" --yes-playlist -f "bv*+ba/b" --merge-output-format ${format} --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json --convert-thumbnails png --ignore-errors --no-abort-on-error --retries 10 --fragment-retries 10 --download-archive "${Cache}\\Videos.txt" --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 15 -o "Download/Videos/%(uploader)s/%(title)s.%(ext)s" "${url}"`, { maxBuffer: 1024 * 1024 * 50 })
-                Spinner.succeed(`--==-- \x1b[32m${language[settings.language].Videos.Spinner.FullDownloads}\x1b[0m --==--`)
-                await WaitSeconds(3000)
-                console.clear()
-            }
-
-            if (Verify.Type == "file") {
-                Spinner.color = "yellow"
-                Spinner.text = `--> \x1b[32m${language[settings.language].Videos.Spinner.DownloadingVideos}\x1b[0m <--`
-                try {
-                    await execPromise(`"${utils}\\yt-dlp.exe" --ffmpeg-location "${utils}" -a ${url} --yes-playlist -f "bv*+ba/b" --merge-output-format ${format} --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json --convert-thumbnails png --ignore-errors --no-abort-on-error --retries 10 --fragment-retries 10 --download-archive "${Cache}\\Videos.txt" --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 15 -o "Download/Videos/%(uploader)s/%(title)s.%(ext)s"`, { maxBuffer: 1024 * 1024 * 50 })
-                }
-                catch {
-                    Spinner.fail(language[settings.language].Error.Default)
-                    await WaitSeconds(3000)
-                    console.clear()
-                    if (Retry > 0) {
-                        Retry--
-                        console.log(`\x1b[37m --==--  \x1b[31m${language[settings.language].Error.Retry}\x1b[37m --==-- \x1b[0m`)
-                        await WaitSeconds(3000)
-                        console.clear()
-                        await WaitSeconds(500)
-                        this.DownloadVideos({ format, url })
-                    } else {
-                        console.log(`\x1b[37m --==--  \x1b[31m${language[settings.language].Error.MaxRetry}\x1b[37m --==-- \x1b[0m`)
-                        await WaitSeconds(5000)
-                        Retry = 3
-                        console.clear()
-                    }
-                }
-                // await execPromise(await execPromise(`"${utils}\\yt-dlp.exe" -a ${url} --ffmpeg-location "${utils}\\ffmpeg.exe" --yes-playlist -f "bv*+ba/b" --merge-output-format ${format} --embed-thumbnail --embed-metadata --embed-chapters --embed-info-json --convert-thumbnails png --ignore-errors --no-abort-on-error --retries 10 --fragment-retries 10 --download-archive "${Cache}\\Videos.txt" --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 15 -o "Download/Videos/%(uploader)s/%(title)s.%(ext)s"`))
-                Spinner.succeed(`--==-- \x1b[32m${language[settings.language].Videos.Spinner.FullDownloads}\x1b[0m --==--`)
-                await WaitSeconds(3000)
-                console.clear()
-            }
-        }
-    },
-
+    Donwload
 }
